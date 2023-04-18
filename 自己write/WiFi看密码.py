5e1f630058e9966a7d3a01d61fe18fd935fb3bc2
YC2023 <cgl@88.com>
2023-04-18 13:28:55 +0800

    """面向win10 CMD->有中文会导致不能对齐 有一个中文就减少一个对齐的量"""

--- 自己write/WiFi看密码.py
+++ 自己write/WiFi看密码.py
@@ -3,6 +3,17 @@
 # 时 间：2023/4/9 5:40
 import subprocess
 import re
+
+
+def cn(str1):
+    """面向win10 CMD->有中文会导致不能对齐 有一个中文就减少一个对齐的量"""
+    count = 0
+    for char in str1:
+        if '\u4e00' <= char <= '\u9fff':
+            count += 1
+    return count
+
+
 output = subprocess.run(['netsh', 'wlan', 'show', 'profiles'], capture_output=True).stdout.decode('gbk')
 # print(output)
 wifis = [wifi.strip() for wifi in re.findall(r'所有用户配置文件\s*:\s*(.+)', output)]
@@ -16,9 +27,9 @@ for wifi in wifis:
     if password:
         password = password.group(1).strip().split("\\r\\n\\r\\n")[0]
     else:
-        password = None
+        password = ' '
 
-    print("▓ "+f'{wifi.ljust(18, " ")}▓  {password}'.ljust(40, " ")+"▓")
+    print("▓ "+f'{wifi.ljust(18-cn(wifi), " ")}▓  {password}'.ljust(40-cn(wifi), " ")+"▓")
 input("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ 回车退出▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n公众号：仰晨")
 
 
