a3f7a3ead9a4a24b2a4f3dfd1fd21388cf0adeb2
YC2023 <cgl@88.com>
2023-05-29 03:24:22 +0800

    py也是强类型

--- test/py明确类型.py
+++ test/py明确类型.py
@@ -2,9 +2,26 @@
 # 文件名：py明确类型
 # 时 间：2023/5/29 3:09
 
+from typing import List
 
 i: int = 1
 
 i = "aaa11"
 
 print(i)
+
+
+def greeting(name: str, age: int) -> str:
+    return f"Hello, {name}! You are {age} years old."
+
+
+def sum_numbers(numbers: List[int]) -> int:
+    return sum(numbers)
+
+
+name = "Alice"
+age = 30
+print(greeting(name, age))
+
+numbers = [1, 2, 3, 4, 5]
+print(sum_numbers(numbers))
